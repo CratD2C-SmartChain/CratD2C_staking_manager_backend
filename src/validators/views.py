1c48c221646eb9e43f212c27d9b1c6d3e5ecb097
longwinterday <longwinterday@gmail.com>
2024-07-30 13:37:50 +0300

    add detail

--- src/validators/views.py
+++ src/validators/views.py
@@ -48,7 +48,8 @@ class ValidatorView(ListCreateAPIView):
     )
     def post(self, request):
         serializer = ValidatorCreateSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        if not serializer.is_valid():
+            return Response(data={"detail": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         serializer.save()
         instance = Validator.objects.get(address=serializer.validated_data['address'])
         serializer = ValidatorSerializer(instance=instance)
