3408c7bf3c9f4454758dbdccc85e9e0c9a7f8172
longwinterday <longwinterday@gmail.com>
2024-08-02 13:37:05 +0300

    fix interval

--- src/statistic/utils.py
+++ src/statistic/utils.py
@@ -11,7 +11,7 @@ class IntervalProcessor:
         current_checkpoint_block = last_block - last_block % config.BLOCKCHAIN.EPOCH_LEN
         if current_checkpoint_block < config.BLOCKCHAIN.EPOCH_LEN:
             return 0
-        previous_checkpoint_block = last_block - config.BLOCKCHAIN.EPOCH_LEN
+        previous_checkpoint_block = current_checkpoint_block - config.BLOCKCHAIN.EPOCH_LEN
         current_checkpoint_ts = self.network.rpc.eth.get_block(current_checkpoint_block).timestamp
         previous_checkpoint_ts = self.network.rpc.eth.get_block(previous_checkpoint_block).timestamp
         return current_checkpoint_ts - previous_checkpoint_ts
