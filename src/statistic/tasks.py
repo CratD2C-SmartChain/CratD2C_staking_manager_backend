0a98c509f0849c1da8a698fb26e5f8de9d47d504
longwinterday <longwinterday@gmail.com>
2024-08-16 12:44:06 +0300

    add delegators info

--- src/statistic/tasks.py
+++ src/statistic/tasks.py
@@ -9,7 +9,7 @@ def update_total_delegators():
     validators, _ = contract_processor.get_active_validators_info()
     delegators = set()
     for validator in validators:
-        delegators.add(contract_processor.get_delegator_info_per_validator(validator))
+        delegators.update(contract_processor.get_delegator_info_per_validator(validator))
     delegators_info, _ = DelegatorsInfo.objects.get_or_create()
     delegators_info.total_delegators = len(delegators)
     delegators_info.save()
