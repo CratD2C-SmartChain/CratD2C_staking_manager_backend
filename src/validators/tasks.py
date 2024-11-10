a7f834a7bd8c3acd55a68a3fa24a413e25a453dc
“qwdqwqdwqde” <yulian.khalitov@gmail.com>
2024-11-10 21:07:14 +0300

    F-2024-6602 | Lack of Concurrency Control in Validator Update

--- src/validators/tasks.py
+++ src/validators/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from django.db import transaction
 
 from src.validators.utils import contract_processor
 from src.validators.models import Validator
@@ -9,12 +10,13 @@ def update_active_validators_amounts():
     validators, amount = contract_processor.get_active_validators_info()
     data = zip(validators, amount)
     for address, amount in data:
-        validator = Validator.objects.filter(address=address).first()
-        if validator:
-            validator.stake_amount = amount
-            if validator.status != Validator.ValidatorStatus.HEALTHY:
-                validator.status = Validator.ValidatorStatus.HEALTHY
-            validator.save()
+        with transaction.atomic():
+            validator = Validator.objects.select_for_update().filter(address=address).first()
+            if validator:
+                validator.stake_amount = amount
+                if validator.status != Validator.ValidatorStatus.HEALTHY:
+                    validator.status = Validator.ValidatorStatus.HEALTHY
+                validator.save()
 
     db_validators = Validator.objects.filter(
         status=Validator.ValidatorStatus.HEALTHY).values_list('address', flat=True)
@@ -22,26 +24,29 @@ def update_active_validators_amounts():
     validators_db_set = set(db_validators)
     difference = validators_db_set.difference(validators_contract_set)
     for address in difference:
-        validator = Validator.objects.filter(address=address).first()
-        if validator:
-            validator.status = Validator.ValidatorStatus.STOPPED
-            validator.save()
+        with transaction.atomic():
+            validator = Validator.objects.select_for_update().filter(address=address).first()
+            if validator:
+                validator.status = Validator.ValidatorStatus.STOPPED
+                validator.save()
 
 
 @shared_task(name="update_archived_validators")
 def update_archived_validators():
     validators, amounts = contract_processor.get_stopped_validators_info()
     for i, validator in enumerate(validators):
-        db_validator = Validator.objects.filter(address__iexact=validator).first()
-        if db_validator:
-            db_validator.stake_amount = sum(amounts[i])
-            db_validator.save()
+        with transaction.atomic():
+            db_validator = Validator.objects.select_for_update().filter(address__iexact=validator).first()
+            if db_validator:
+                db_validator.stake_amount = sum(amounts[i])
+                db_validator.save()
     db_validators = Validator.objects.filter(status=Validator.ValidatorStatus.STOPPED).values_list('address', flat=True)
     validators_db_set = set(db_validators)
     difference = validators_db_set.difference(validators)
     for address in difference:
-        validator = Validator.objects.filter(address__iexact=address).first()
-        if validator:
-            is_validator = contract_processor.is_validator_active(address)
-            validator.status = Validator.ValidatorStatus.HEALTHY if is_validator else Validator.ValidatorStatus.ARCHIVED
-            validator.save()
+        with transaction.atomic():
+            validator = Validator.objects.select_for_update().filter(address__iexact=address).first()
+            if validator:
+                is_validator = contract_processor.is_validator_active(address)
+                validator.status = Validator.ValidatorStatus.HEALTHY if is_validator else Validator.ValidatorStatus.ARCHIVED
+                validator.save()
